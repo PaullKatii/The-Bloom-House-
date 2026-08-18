94cd1e45959922f0716f1fc078fe74e7a930b4da
PaullKatii <antonkatipana@gmail.com>
2026-08-18 18:47:55 +0900

    Create config.js

---
+++ firebase/config.js
@@ -0,0 +1,13 @@
+// Firebase Web App configuration for The Bloom House.
+// Web API keys are intended to be used by the client application.
+// Never put a Firebase service-account private key in this file.
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQw0vy2PYxuhOE5JRpswgVKbfUmHzSNlc",
+  authDomain: "the-bloom-house-a6345.firebaseapp.com",
+  projectId: "the-bloom-house-a6345",
+  storageBucket: "the-bloom-house-a6345.firebasestorage.app",
+  messagingSenderId: "979046341886",
+  appId: "1:979046341886:web:49494640502aa159fc99af",
+  measurementId: "G-3T6DE1WN72"
+};
